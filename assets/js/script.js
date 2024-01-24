4e1a3e404c31c0f80d7441b3c2da6c34ed42ea13
danimisme <msubhanr53@gmail.com>
2024-01-24 23:52:56 +0700

    menambahkan animate css ke halaman beranda (index.html)

---
+++ assets/js/script.js
@@ -0,0 +1,110 @@
+$(document).ready(function () {
+  $(".header_title").waypoint(
+    function () {
+      $(".header_title").addClass("animate__animated animate__fadeInLeft");
+    },
+    { offset: "80%" }
+  );
+  $(".header_caption").waypoint(
+    function () {
+      $(".header_caption").addClass("animate__animated animate__fadeInRight");
+    },
+    { offset: "80%" }
+  );
+
+  $(".card_title").waypoint(
+    function () {
+      $(".card_title").addClass("animate__animated animate__fadeInLeft");
+    },
+    { offset: "80%" }
+  );
+
+  $(".card_list_1").waypoint(
+    function () {
+      $(".card_list_1").addClass("animate__animated animate__zoomIn");
+    },
+    { offset: "80%" }
+  );
+
+  $(".card_list_2").waypoint(
+    function () {
+      $(".card_list_2").addClass("animate__animated animate__zoomIn");
+    },
+    { offset: "80%" }
+  );
+
+  $(".card_list_3").waypoint(
+    function () {
+      $(".card_list_3").addClass("animate__animated animate__zoomIn");
+    },
+    { offset: "80%" }
+  );
+
+  $(".button_menu").waypoint(
+    function () {
+      $(".button_menu").addClass("animate__animated animate__swing");
+    },
+    { offset: "80%" }
+  );
+
+  $(".image_content_1").waypoint(
+    function () {
+      $(".image_content_1").addClass("animate__animated animate__zoomIn");
+    },
+    { offset: "80%" }
+  );
+
+  $(".title_content_1").waypoint(
+    function () {
+      $(".title_content_1").addClass("animate__animated animate__fadeInRight");
+    },
+    { offset: "80%" }
+  );
+
+  $(".article_content_1").waypoint(
+    function () {
+      $(".article_content_1").addClass("animate__animated animate__fadeInUp");
+    },
+    { offset: "80%" }
+  );
+
+  $(".image_content_2").waypoint(
+    function () {
+      $(".image_content_2").addClass("animate__animated animate__zoomIn");
+    },
+    { offset: "80%" }
+  );
+
+  $(".title_content_2").waypoint(
+    function () {
+      $(".title_content_2").addClass("animate__animated animate__fadeInDown");
+    },
+    { offset: "80%" }
+  );
+
+  $(".article_content_2").waypoint(
+    function () {
+      $(".article_content_2").addClass("animate__animated animate__fadeInUp");
+    },
+    { offset: "80%" }
+  );
+
+  $(".info_list_1").waypoint(
+    function () {
+      $(".info_list_1").addClass("animate__animated animate__zoomIn");
+    },
+    { offset: "80%" }
+  );
+  $(".info_list_2").waypoint(
+    function () {
+      $(".info_list_2").addClass("animate__animated animate__zoomIn");
+    },
+    { offset: "80%" }
+  );
+  $(".info_list_3").waypoint(
+    function () {
+      $(".info_list_3").addClass("animate__animated animate__zoomIn");
+    },
+    { offset: "80%" }
+  );
+});
